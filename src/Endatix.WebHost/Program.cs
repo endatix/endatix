3d7345306caa797a5ccc23d141a4e646c343d524
Peter Marinov <peter.marinov@americaneagle.com>
2024-08-19 16:33:23 +0300

    Removing commented code to clean up src/Endatix.WebHost/Program.cs; removing not needed dependencies

--- src/Endatix.WebHost/Program.cs
+++ src/Endatix.WebHost/Program.cs
@@ -6,16 +6,6 @@ var endatixBuilder = builder.CreateEndatix()
                             .AddDefaultSetup()
                             .AddApiEndpoints();
 
-// TODO: expose custom plugin logic for plugin custom domain event handlers 
-// builder.Services.AddMediatRInfrastructure(options =>
-//                 {
-//                     options.UsePipelineLogging();
-//                     options.AdditionalAssemblies =
-//                         [
-//                             Endatix.Samples.CustomEventHandlers.AssemblyReference.Assembly
-//                         ];
-//                 });
-
 var app = builder.Build();
 
 app.UseEndatixMiddleware()
