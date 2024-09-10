fb18f7a8172cff2763fb0809f685be5fb1a0259d
Oggy Shopov <oggy@endatix.com>
2024-09-10 17:38:37 +0300

    Removed environment variables file and added env to gitignore

--- samples/Endatix.Samples.WebApp/Pages/Form.cshtml.cs
+++ samples/Endatix.Samples.WebApp/Pages/Form.cshtml.cs
@@ -1,34 +1,26 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Options;
-using Endatix.Samples.WebApp.ApiClient;
-using Endatix.Samples.WebApp.ApiClient.Model.Responses;
 
 namespace Endatix.Samples.WebApp.Pages;
 
 public class FormModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private readonly IEndatixClient _client;
-    private readonly HttpClientOptions _settings;
+    private readonly IEndatixPlatform _endatix;
 
     public string FormId { get; set; }
     public FormDefinitionResponse Form { get; set; }
 
-    public readonly string BaseUrl;
-
-    public FormModel(ILogger<IndexModel> logger, IEndatixClient client, IOptions<HttpClientOptions> options)
+    public FormModel(ILogger<IndexModel> logger, IEndatixPlatform endatix)
     {
         _logger = logger;
-        _client = client;
-        _settings = options.Value;
-
-        BaseUrl = _settings.ApiBaseUrl;
+        _endatix = endatix;
     }
 
     public async Task OnGetAsync(long id, CancellationToken cancellationToken)
     {
         FormId = id.ToString();
-        FormDefinitionResponse form = await _client.GetActiveDefinitionAsync(id, cancellationToken);
+        
+        FormDefinitionResponse form = await _endatix.Forms.GetActiveDefinitionAsync(id, cancellationToken);
 
         _logger.LogInformation("Form fetching complete. Results is {@response}", form);
         if (form != null)
