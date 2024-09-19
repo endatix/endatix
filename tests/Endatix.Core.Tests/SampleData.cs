61975a1025f6e605a22fb3fd5650bf7ff8b5a5c4
Dobromir Petrov <dspetrov@users.noreply.github.com>
2024-09-20 00:06:27 +0300

    fix: Set Submission CompletedAt time correctly

--- tests/Endatix.Core.Tests/SampleData.cs
+++ tests/Endatix.Core.Tests/SampleData.cs
@@ -4,4 +4,6 @@ public static class SampleData
 {
     public const string FORM_DEFINITION_JSON_DATA_1 = "{\"type\":\"text\",\"name\":\"firstname\"";
     public const string FORM_DEFINITION_JSON_DATA_2 = "{\"type\":\"text\",\"name\":\"lastname\"";
+    public const string SUBMISSION_JSON_DATA_1 = "{\"firstname\":\"mr test\"";
+    public const string SUBMISSION_JSON_DATA_2 = "{\"firstname\":\"mrs test\"";
 }
