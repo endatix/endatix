73fd5490cbda2bce7b25ffc893bdc9ebdb3d79e4
Peter Marinov <peter@endatix.com>
2024-09-20 15:37:03 +0300

    Appplying Permission authorization for the newly added endpoints

--- src/Endatix.Api/Endpoints/Forms/Update.cs
+++ src/Endatix.Api/Endpoints/Forms/Update.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Endatix.Api.Infrastructure;
 using Endatix.Core.Entities;
 using Endatix.Core.UseCases.Forms.Update;
+using Endatix.Infrastructure.Identity.Authorization;
 
 namespace Endatix.Api.Endpoints.Forms;
 
@@ -18,7 +19,7 @@ public class Update(IMediator _mediator) : Endpoint<UpdateFormRequest, Results<O
     public override void Configure()
     {
         Put("forms/{formId}");
-        Roles("Admin");
+        Permissions(Allow.AllowAll);
         Summary(s =>
         {
             s.Summary = "Update a form";
