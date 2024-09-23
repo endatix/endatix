fa21a5975af9a7a97ac682820d137795641865e2
CALIBRA\oggy <oggy@surveysocket.com>
2024-09-23 11:36:03 +0300

    WIP

---
+++ apps/endatix-hub/components/survey.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Model } from 'survey-core'
+import { Survey } from 'survey-react-ui'
+import 'survey-core/defaultV2.css'
+// import { json } from '../../data/survey_json.js'
+  
+export default function SurveyComponent() {
+  const model = new Model("{}");
+  return (
+    <Survey model={model}/>
+  );
+}
