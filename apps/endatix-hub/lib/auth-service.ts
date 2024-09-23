aa223aa79f29496f8acf6c48ef3d3b97a3acc045
Peter Marinov <peter@endatix.com>
2024-09-23 10:09:50 +0300

    Adding ability to decode JWT. Refactoring WIP with Jose, so we can control the expiration time and decrease the number of 3rd party dependancies

--- apps/endatix-hub/lib/auth-service.ts
+++ apps/endatix-hub/lib/auth-service.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import { SignJWT, jwtVerify } from "jose";
 import { getIronSession, SessionOptions } from "iron-session";
 import { cookies } from "next/headers";
 import { revalidatePath } from "next/cache";
+import { decodeJwt } from "jose";
 
 interface SessionData {
     username: string;
@@ -25,6 +27,25 @@ const sessionOptions: SessionOptions = {
     },
 };
 
+const securityOptions = {
+    secretKey: new TextEncoder().encode(`${process.env.SESSION_SECRET}`)
+};
+
+export const encryptToken = async (payload: any) => {
+    return await new SignJWT(payload)
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("10 sec from now")
+        .sign(securityOptions.secretKey);
+}
+
+export const decryptToken = async (token: string): Promise<any> => {
+    const { payload } = await jwtVerify(token, securityOptions.secretKey, {
+        algorithms: ["HS256"],
+    });
+    return payload;
+}
+
 export const getSession = async () => {
     const session = await getIronSession<SessionData>(cookies(), sessionOptions);
 
@@ -40,6 +61,12 @@ export const getSession = async () => {
 export const login = async (token: string, username: string) => {
     const session = await getSession();
 
+    const jwtToken = decodeJwt(token);
+    if (!jwtToken.exp) {
+        return;
+    }
+
+    const expirationDate = new Date(jwtToken.exp * 1000)
     session.token = token;
     session.username = username;
     session.isLoggedIn = true;
@@ -50,8 +77,8 @@ export const login = async (token: string, username: string) => {
 }
 
 //const logout = async () =>  { 
- //   const session = await getSession();
- //   session.destroy();
+//   const session = await getSession();
+//   session.destroy();
 //
- //   revalidatePath("/login");
- // }
+//   revalidatePath("/login");
+// }
