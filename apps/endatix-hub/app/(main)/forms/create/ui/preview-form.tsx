21baf469e38c21429fec8d8a3887d2f4de9970bf
Peter Marinov <peter@endatix.com>
2024-10-21 09:44:59 +0300

    Adding Survey Creator to load the survey

--- apps/endatix-hub/app/(main)/forms/create/ui/preview-form.tsx
+++ apps/endatix-hub/app/(main)/forms/create/ui/preview-form.tsx
@@ -1,19 +1,40 @@
 "use client"
 
 import { Model } from 'survey-core'
-import { Survey } from 'survey-react-ui'
+import { ICreatorOptions } from 'survey-creator-core'
+import { SurveyCreatorComponent, SurveyCreator } from 'survey-creator-react'
 import 'survey-core/defaultV2.css'
+import 'survey-creator-core/survey-creator-core.css'
+import { useEffect, useState } from 'react'
 
 interface PreviewFormProps {
     model: Model | null;
 }
 
-const PreviewForm = async ({ model }: PreviewFormProps) => {
-    const onFormComplete = async (sender: Model) => {
-        console.log(sender.data);
-    }
+const creatorOptions: ICreatorOptions = {
+    showPreview: true,
+    showTranslationTab: false,
+    showDesignerTab: false
+};
+
+const PreviewForm = ({ model }: PreviewFormProps) => {
+    const [creator, setCreator] = useState<SurveyCreator | null>(null);
+    useEffect(() => {
+        if (creator) {
+            return;
+        }
+
+        const newCreator = new SurveyCreator(creatorOptions);
+        newCreator.JSON = model?.toJSON();
+        newCreator.saveSurveyFunc = (no: number, callback: (num: number, status: boolean) => void) => {
+            console.log(JSON.stringify(newCreator?.JSON));
+            callback(no, true);
+        };
+        setCreator(newCreator);
+    }, []);
+
     return (
-        model && <Survey model={model} onComplete={onFormComplete} />
+        creator && <SurveyCreatorComponent creator={creator} />
     );
 }
 
