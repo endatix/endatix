9b50358698739ea2d99656ec5d9ebe9b78c48843
Peter Marinov <peter@endatix.com>
2024-10-30 13:35:46 +0200

    Adding Workflow menu item. Increase main menu icon size

--- apps/endatix-hub/components/layout-ui/navigation/main-nav.tsx
+++ apps/endatix-hub/components/layout-ui/navigation/main-nav.tsx
@@ -16,16 +16,16 @@ const MainNav = () => {
   const settingsNavItem = sitemap.Settings;
   return (
     <TooltipProvider>
-      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5 space-y-1">
         <NavLink
           path={logo.path}
           text={logo.text}
           setIsActive={false}
-          className="group flex h-9 w-9 shrink-0 items-center justify-center rounded-sm gap-2 bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+          className="group flex h-10 w-10 shrink-0 items-center justify-center rounded-sm gap-2 bg-primary text-lg font-semibold text-primary-foreground md:h-9 md:w-9 md:text-base"
         >
           <Image
             aria-hidden
-            className="h-8 w-8 transition-all group-hover:scale-110 rounded-sm"
+            className="h-9 w-9 transition-all group-hover:scale-110 rounded-sm"
             src={EndatixLogoSvg}
             alt="logo"
           />
@@ -34,7 +34,7 @@ const MainNav = () => {
           <Tooltip key={navItem.text}>
             <TooltipTrigger asChild={false}>
               <NavLink path={navItem.path} text={navItem.text}>
-                <navItem.IconType className="h-5 w-5" />
+                <navItem.IconType className="h-6 w-6" />
               </NavLink>
             </TooltipTrigger>
             <TooltipContent side="right">{navItem.text}</TooltipContent>
@@ -45,7 +45,7 @@ const MainNav = () => {
         <Tooltip>
           <TooltipTrigger asChild={false}>
             <NavLink path={settingsNavItem.path} text={settingsNavItem.text}>
-              <settingsNavItem.IconType className="h-5 w-5" />
+              <settingsNavItem.IconType className="h-6 w-6" />
             </NavLink>
           </TooltipTrigger>
           <TooltipContent side="right">{settingsNavItem.text}</TooltipContent>
