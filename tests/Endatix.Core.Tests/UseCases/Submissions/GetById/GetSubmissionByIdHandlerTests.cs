f056427d6a4fc9d6db6a6b00a255776693e6b7a1
Dobromir Petrov <14013810+dspetrov@users.noreply.github.com>
2024-12-06 16:51:15 +0200

    feat: Cover submissions commands and queries with unit tests (#264)
    Co-authored-by: Peter Marinov <peter@endatix.com>
    Co-authored-by: Dobromir Petrov <dspetrov@users.noreply.github.com>

---
+++ tests/Endatix.Core.Tests/UseCases/Submissions/GetById/GetSubmissionByIdHandlerTests.cs
@@ -0,0 +1,63 @@
+using Endatix.Core.Entities;
+using Endatix.Core.Infrastructure.Domain;
+using Endatix.Core.Infrastructure.Result;
+using Endatix.Core.Specifications;
+using Endatix.Core.UseCases.Submissions.GetById;
+
+namespace Endatix.Core.Tests.UseCases.Submissions.GetById;
+
+public class GetSubmissionByIdHandlerTests
+{
+    private readonly IRepository<Submission> _submissionsRepository;
+    private readonly GetByIdHandler _handler;
+
+    public GetSubmissionByIdHandlerTests()
+    {
+        _submissionsRepository = Substitute.For<IRepository<Submission>>();
+        _handler = new GetByIdHandler(_submissionsRepository);
+    }
+
+    [Fact]
+    public async Task Handle_SubmissionNotFound_ReturnsNotFoundResult()
+    {
+        // Arrange
+        var request = new GetByIdQuery(1, 1);
+        _submissionsRepository.SingleOrDefaultAsync(
+            Arg.Any<SubmissionWithDefinitionSpec>(), 
+            Arg.Any<CancellationToken>())
+            .Returns((Submission?)null);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Status.Should().Be(ResultStatus.NotFound);
+        result.Errors.Should().Contain("Submission not found");
+    }
+
+    [Fact]
+    public async Task Handle_ValidRequest_ReturnsSubmission()
+    {
+        // Arrange
+        var formId = 1L;
+        var formDefinitionId = 1L;
+        var submissionId = 1L;
+        var submission = new Submission("{ }", formId, formDefinitionId) { Id = submissionId };
+        var request = new GetByIdQuery(formId, submissionId);
+        
+        _submissionsRepository.SingleOrDefaultAsync(
+            Arg.Any<SubmissionWithDefinitionSpec>(), 
+            Arg.Any<CancellationToken>())
+            .Returns(submission);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Status.Should().Be(ResultStatus.Ok);
+        result.Value.Should().NotBeNull();
+        result.Value.Should().Be(submission);
+    }
+}
