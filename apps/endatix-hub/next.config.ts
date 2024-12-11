374a17a3d3c13e7169238efd4172a1146ad09dd0
Peter Marinov <peter@endatix.com>
2024-12-11 18:02:48 +0200

    Adding redirects to follow docs

--- apps/endatix-hub/next.config.ts
+++ apps/endatix-hub/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone", // Used to decrease the size of the application, check https://nextjs.org/docs/pages/api-reference/next-config-js/output
+  async redirects() {
+    return [
+      {
+        source: '/((?!.swa).*)about',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
