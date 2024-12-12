0650d18056d71b4e4851659a005ae9798c2a6ce6
Peter Marinov <peter@endatix.com>
2024-12-12 18:13:23 +0200

    Adding zod and @tanstack/react-table Parsing test paging params

--- apps/endatix-hub/app/(main)/forms/submissions/[formId]/page.tsx
+++ apps/endatix-hub/app/(main)/forms/submissions/[formId]/page.tsx
@@ -1,18 +1,47 @@
 import PageTitle from '@/components/headings/page-title';
 import { getForm, getSubmissions } from '@/services/api';
 import SubmissionsTable from './ui/submissions-table';
+import type { Metadata, ResolvingMetadata } from 'next'
 
 type Params = {
   params: Promise<{ formId: string }>
-};
+  searchParams: Promise<{ page: string, pageSize: string }>
+}
 
-async function ResponsesPage({ params }: Params) {
+export async function generateMetadata(
+  { params, searchParams }: Params,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
   const { formId } = await params;
+  const form = await getForm(formId);
 
+  return {
+    title: `Submissions for ${form.name}`,
+    description: `View all submissions for ${form.name}`,
+    openGraph: {
+      title: `Search params: ${JSON.stringify((await searchParams))}`,
+      description: `Parent title: ${(await parent).title}`,
+    },
+  };
+}
+
+const logSearchParams = async (searchParams: Promise<{ page: string, pageSize: string }>) => {
+  const { page, pageSize } = await searchParams;
+  if (page) {
+    console.log("page", page);
+  }
+  if (pageSize) {
+    console.log("pageSize", pageSize);
+  }
+}
+
+async function ResponsesPage({ params, searchParams }: Params) {
+  const { formId } = await params;
   const [submissions, form] = await Promise.all([
     await getSubmissions(formId),
     await getForm(formId)
   ]);
+  await logSearchParams(searchParams);
 
   return (
     <>
