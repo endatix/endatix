9639f0a1c7cf94eb7cd1590119822d3380d0e160
Peter Marinov <peter@endatix.com>
2024-12-16 16:47:12 +0200

    Revert "Adding current state: form sheet update with WIP"
    This reverts commit fbbeea9c26e1196bc6115e9084332aa4e0cb0027.

--- apps/endatix-hub/app/(main)/forms/ui/form-sheet.tsx
+++ apps/endatix-hub/app/(main)/forms/ui/form-sheet.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button"
-import { ArrowRight, Eye, Pencil } from 'lucide-react';
+import { ArrowRight, Eye, Pencil, Trash } from 'lucide-react';
 import {
     Sheet,
     SheetContent,
@@ -12,34 +12,25 @@ import {
 } from "@/components/ui/sheet";
 import { Form } from "@/types";
 import Link from "next/link";
-import { SectionTitle } from "@/components/headings/section-title";
-import { Switch } from "@/components/ui/switch";
-import { Label } from "@/components/ui/label";
-import { useTransition, useState } from "react";
-import { updateFormStatusAction } from "../[formId]/update-form-status.action";
+import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
 import { toast } from "sonner";
 
 type FormSheetProps = {
-    selectedForm: Form | null,
-    onFormUpdate: (updatedForm: Form) => Promise<void>;
+    selectedForm: Form | null
 }
 
-const FormSheet = ({
-    selectedForm,
-    onFormUpdate }: FormSheetProps) => {
-    const [pending, startTransition] = useTransition();
-    const [isEnabled, setIsEnabled] = useState(selectedForm?.isEnabled ?? false);
-
-    if (!selectedForm) {
-        return null;
-    }
-
+const FormSheet = ({ selectedForm }: FormSheetProps) => {
     const getFormattedDate = (date?: Date) => {
         if (!date) {
             return;
         }
 
         return new Date(date).toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', month: '2-digit', day: '2-digit', year: 'numeric', hour12: true });
+    }
+
+    const getFormatDescription = (): string => {
+        return selectedForm?.description ?? "This ensures that the first <span> is treated as an inline-block element, which helps maintain its visibility and allows it to occupy space even if its height or width is small. You can also consider adding a min-w or min-h class if you want to enforce a specific size.";
     }
 
     const getSubmissionsLabel = () => {
@@ -55,33 +46,19 @@ const FormSheet = ({
         return (selectedForm?.submissionsCount && selectedForm.submissionsCount > 0) ?? false;
     }
 
-    const toggleEnabled = async (enabled: boolean) => {
-        setIsEnabled(enabled); // Optimistically update UI
-        startTransition(async () => {
-            try {
-                await updateFormStatusAction(selectedForm.id, enabled);
-                await onFormUpdate(selectedForm);
-                toast(`Form is now ${enabled ? "enabled" : "disabled"}`);
-            } catch (error) {
-                setIsEnabled(!enabled);
-                toast.error("Failed to update form status");
-            }
-        });
-    }
-
     return (
         selectedForm && (
             <Sheet modal={false} open={selectedForm != null} >
                 <SheetContent className="w-[600px] sm:w-[480px] sm:max-w-none">
                     <SheetHeader>
-                        <SheetTitle className="text-2xl font-bold">
+                        <SheetTitle>
                             {selectedForm?.name}
                         </SheetTitle>
                         <SheetDescription>
                             {selectedForm?.description}
                         </SheetDescription>
                     </SheetHeader>
-                    <div className="my-8 flex space-x-2 justify-end">
+                    <div className="my-8 flex space-x-2">
                         <Link href={`forms/${selectedForm.id}`}>
                             <Button variant={"outline"}>
                                 <Pencil className="mr-2 h-4 w-4" />
@@ -94,8 +71,33 @@ const FormSheet = ({
                                 Preview
                             </Button>
                         </Link>
+                        <Link href="#" onClick={() => toast("Coming soon")}>
+                            <Button variant={"outline"}>
+                                <Trash className="mr-2 h-4 w-4" />
+                                Delete
+                            </Button>
+                        </Link>
                     </div>
                     <div className="grid gap-2 py-4">
+                        <div className="grid grid-cols-4 py-2 items-start gap-4">
+                            <span className="text-right self-start">
+                                Description
+                            </span>
+                            <p className="text-sm text-muted-foreground col-span-3">
+                                {getFormatDescription()}
+                            </p>
+                        </div>
+                        <div className="grid grid-cols-4 py-2 items-center gap-4">
+                            <span className="text-right self-start">
+                                Status
+                            </span>
+                            <div className="col-span-3">
+                                <span className={cn("inline-block h-2 w-2 mr-1 rounded-full", selectedForm.isEnabled ? "bg-green-600" : "bg-gray-600")} />
+                                <span className="text-sm text-muted-foreground">
+                                    {selectedForm.isEnabled ? "Enabled" : "Disabled"}
+                                </span>
+                            </div>
+                        </div>
                         <div className="grid grid-cols-4 py-2 items-center gap-4">
                             <span className="text-right self-start">
                                 Created on
@@ -112,36 +114,26 @@ const FormSheet = ({
                                 {getFormattedDate(selectedForm.modifiedAt)}
                             </span>
                         </div>
-
+                        <Separator />
                         <div className="grid grid-cols-4 py-2 items-center gap-4">
                             <span className="text-right self-start">
-                                Status
+                                Submissions
                             </span>
-                            <div className="col-span-3 flex items-center space-x-2">
-                                <Switch
-                                    id="form-status"
-                                    checked={isEnabled}
-                                    onCheckedChange={toggleEnabled}
-                                    disabled={pending}
-                                    aria-readonly />
-                                <Label htmlFor="form-status">
-                                    {isEnabled ? "Enabled" : "Disabled"}
-                                </Label>
+                            <div className="text-sm text-muted-foreground col-span-3">
+                                {getSubmissionsLabel()}
                             </div>
                         </div>
+                        {hasSubmissions() === true && (
+                            <div>
+                                <Link href={`/forms/${selectedForm.id}/submissions`}>
+                                    <Button variant={"ghost"}>
+                                        <ArrowRight className="mr-2 h-4 w-4" />
+                                        View Submissions
+                                    </Button>
+                                </Link>
+                            </div>
+                        )}
                     </div>
-                    <SectionTitle title="Sharing" headingClassName="text-xl" />
-                    <SectionTitle title="Submissions" headingClassName="text-xl" />
-                    {hasSubmissions() === true && (
-                        <div>
-                            <Link href={`/forms/${selectedForm.id}/submissions`}>
-                                <Button variant={"ghost"}>
-                                    <ArrowRight className="mr-2 h-4 w-4" />
-                                    View Submissions
-                                </Button>
-                            </Link>
-                        </div>
-                    )}
                     <SheetFooter>
                     </SheetFooter>
                 </SheetContent>
@@ -151,3 +143,4 @@ const FormSheet = ({
 }
 
 export default FormSheet;
+
--- apps/endatix-hub/app/(main)/forms/ui/forms-list.tsx
+++ apps/endatix-hub/app/(main)/forms/ui/forms-list.tsx
@@ -3,39 +3,58 @@
 import { Form } from "@/types";
 import FormCard from "./form-card";
 import FormSheet from "./form-sheet";
-import { useState} from "react";
-import { useRouter } from "next/router";
+import { useEffect, useState, useMemo } from "react";
 
 type FormDataProps = {
     forms: Form[];
 };
 
 const FormsList = ({ forms }: FormDataProps) => {
-    const [formsList, setFormsList] = useState<Form[]>(forms);
-    const [selectedForm, setSelectedForm] = useState<Form | null>(null);
+    const [selectedFormId, setSelectedFormId] = useState<string | null>(null);
 
-    const handleFormUpdate = async (updatedForm: Form) => {
-        const router = useRouter();
-        await Promise.resolve(() => router.reload());
-    };
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (!selectedFormId) {
+                return;
+            }
+
+            if (e.key === "Escape") {
+                setSelectedFormId(null); // Deselect
+            } else if (selectedFormId) {
+                const currentIndex = forms.findIndex(form => form.id === selectedFormId);
+                if (e.key === "ArrowUp" || e.key === "ArrowRight") {
+                    const prevIndex = (currentIndex > 0 ? currentIndex - 1 : forms.length - 1);
+                    setSelectedFormId(forms[prevIndex].id);
+                } else if (e.key === "ArrowDown" || e.key === "ArrowLeft") {
+                    const nextIndex = (currentIndex < forms.length - 1 ? currentIndex + 1 : 0);
+                    setSelectedFormId(forms[nextIndex].id);
+                }
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [selectedFormId, forms]);
+
+    const selectedForm = useMemo(() => forms.find(form => form.id === selectedFormId), [selectedFormId, forms]);
 
     return (
         <>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-4">
-                {formsList.map((form) => (
+                {forms.map((form) => (
                     <FormCard
                         key={form.id}
                         form={form}
-                        isSelected={form.id === selectedForm?.id}
-                        onClick={() => setSelectedForm(form)}
+                        isSelected={form.id === selectedFormId}
+                        onClick={() => setSelectedFormId(form.id)}
                     />
                 ))}
             </div>
 
             {selectedForm && (
-                <FormSheet 
-                selectedForm={selectedForm}
-                onFormUpdate={handleFormUpdate} />
+                <FormSheet selectedForm={selectedForm} />
             )}
         </>
     );
