72f51db7c5ddf3bbc1ad3a04c21ae9abd5db789b
Oggy Shopov <oggy@endatix.com>
2024-12-16 17:04:54 +0200

    Quick and dirty slack bot authorization flow

--- apps/endatix-hub/app/(public)/slack/page.tsx
+++ apps/endatix-hub/app/(public)/slack/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import { useSearchParams } from "next/navigation"; // Use `next/navigation` instead of `next/router`
-import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation"; 
 import { getSlackBearerToken, sendSlackBearerToken } from "@/services/integrations";
-import { SlackAuthResponse } from "@/types";
 
 export default async function Slack() {
-  const searchParams = useSearchParams(); // To get query params
+  const searchParams = useSearchParams(); 
   const code = searchParams.get("code");
   var message = "";
 
   var responseJson = await getSlackBearerToken(code ?? "");
 
   if(responseJson.ok) {
-    await sendSlackBearerToken(responseJson.access_token);
+    var result = await sendSlackBearerToken(responseJson.access_token);
 
+    if(result) {
+      message = "Successfully received token";
+    }
+    else {
+      message = "Failed to received token";
+    }
   }
   else {
     message = responseJson.error;
@@ -22,7 +26,7 @@ export default async function Slack() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1>Slack authorization for Endatix Bot</h1>
+      <h3>Slack authorization for Endatix Bot</h3>
       {message}<br />
     </div>
   );
--- apps/endatix-hub/services/integrations.ts
+++ apps/endatix-hub/services/integrations.ts
@@ -2,18 +2,17 @@ import { SlackAuthResponse } from "@/types";
 import { HeaderBuilder } from "./header-builder";
 
 const API_BASE_URL = `${process.env.ENDATIX_BASE_URL}/api`;
+const SLACK_CLIENT_ID = `${process.env.SLACK_CLIENT_ID}`;
+const SLACK_CLIENT_SECRET = `${process.env.SLACK_CLIENT_SECRET}`;
+const SLACK_REDIRECT_URI = `${process.env.SLACK_REDIRECT_URI}`;
 
 export const getSlackBearerToken = async (
   code: string
 ): Promise<SlackAuthResponse> => {
   const requestOptions: RequestInit = {};
-  
-  const client_id = "8095858636565.8147306484534";
-  const client_secret = "{slack-app-secret-goes-here}";
-  const redirect_uri = "temp-url"
 
   const response = await fetch(
-    `https://slack.com/api/oauth.v2.access?code=${code}&client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${redirect_uri}`,
+    `https://slack.com/api/oauth.v2.access?code=${code}&client_id=${SLACK_CLIENT_ID}&client_secret=${SLACK_CLIENT_SECRET}&redirect_uri=${SLACK_REDIRECT_URI}`,
     requestOptions
   );
 
@@ -26,7 +25,7 @@ export const getSlackBearerToken = async (
 
 export const sendSlackBearerToken = async (
     token: string
-  ): Promise<string> => {
+  ): Promise<boolean> => {
     const requestOptions: RequestInit = {};
 
     const headers = new HeaderBuilder()
@@ -43,8 +42,8 @@ export const sendSlackBearerToken = async (
       });
   
     if (!response.ok) {
-      throw new Error("error");
+      return false;
     }
   
-    return "ок";
+    return true;
   };
