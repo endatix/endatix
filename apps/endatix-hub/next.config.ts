a805f36baeeb1e213722b4d84e6fa28d4148033d
Peter Marinov <peter@endatix.com>
2024-12-17 15:30:12 +0200

    Increase server limit to 30Mb

--- apps/endatix-hub/next.config.ts
+++ apps/endatix-hub/next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   output: "standalone", // Used to decrease the size of the application, check https://nextjs.org/docs/pages/api-reference/next-config-js/output
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '30mb',
+    },
+  },
   images: {
     remotePatterns: [
       {
