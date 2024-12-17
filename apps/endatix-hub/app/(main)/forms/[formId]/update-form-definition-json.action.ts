220b6eb82094e4aa6edf161d2759ae6f4ed84f9c
Dobromir Petrov <dspetrov@users.noreply.github.com>
2024-12-17 15:11:29 +0200

    feat: Create new forms as draft and do not display drafts on the forms screen

--- apps/endatix-hub/app/(main)/forms/[formId]/update-form-definition-json.action.ts
+++ apps/endatix-hub/app/(main)/forms/[formId]/update-form-definition-json.action.ts
@@ -3,11 +3,11 @@
 import { ensureAuthenticated } from "@/lib/auth-service";
 import { updateFormDefinition } from "@/services/api";
 
-export async function updateFormDefinitionJsonAction(formId: string, formJson: object | null) {
+export async function updateFormDefinitionJsonAction(formId: string, isDraft: boolean, formJson: object | null) {
   await ensureAuthenticated();
 
   try {
-    await updateFormDefinition(formId, JSON.stringify(formJson));
+    await updateFormDefinition(formId, isDraft, JSON.stringify(formJson));
     return { success: true };
   } catch (error) {
     console.error("Failed to update form definition", error);
