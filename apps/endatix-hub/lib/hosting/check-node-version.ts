8914649758a5fee6098e5c126f2cf4872329cb3e
Peter Marinov <peter@endatix.com>
2024-12-31 10:14:28 +0200

    Adding runtime node version check using Next.js Instrumentation and semver

---
+++ apps/endatix-hub/lib/hosting/check-node-version.ts
@@ -0,0 +1,18 @@
+import packageJson from '@/package.json' assert { type: 'json' }
+import semver from 'semver'
+
+function checkNodeVersion() {
+    const nodeRuntimeVersion = process.version;
+    const { engines: { node: engines } } = packageJson;
+
+    if (!semver.satisfies(nodeRuntimeVersion, engines, { includePrerelease: true })) {
+        console.log(`⚠️ Warning: Node version check failed 
+            📦 Current Node version (${nodeRuntimeVersion}) does not match the required version of Node (${engines}). 
+            💡 Check Readme for how to setup the correct Node version. 
+            🔗 More info at https://github.com/endatix/endatix/tree/main/apps/endatix-hub`);
+    } else {
+        console.log(`📦 Node version is ${nodeRuntimeVersion}. Node version check passed ✅`);
+    }
+}
+
+export default checkNodeVersion;
