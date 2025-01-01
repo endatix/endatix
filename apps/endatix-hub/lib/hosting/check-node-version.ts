88833d2758e39b86e4454d1ff1c888ca28e31658
Peter Marinov <peter@endatix.com>
2025-01-01 11:27:13 +0200

    Adding tests

--- apps/endatix-hub/lib/hosting/check-node-version.ts
+++ apps/endatix-hub/lib/hosting/check-node-version.ts
@@ -1,18 +1,37 @@
 import packageJson from '@/package.json' assert { type: 'json' }
 import semver from 'semver'
 
+export interface PackageJson {
+    engines?: {
+        node?: string;
+    };
+}
+
 function checkNodeVersion() {
     const nodeRuntimeVersion = process.version;
-    const { engines: { node: engines } } = packageJson;
+    const { engines } = packageJson as PackageJson;
 
-    if (!semver.satisfies(nodeRuntimeVersion, engines, { includePrerelease: true })) {
-        console.log(`⚠️ Warning: Node version check failed ❌ 
+    if (!engines || !engines.node) {
+        console.log(getSuccessMessage(nodeRuntimeVersion));
+        return;
+    }
+
+    if (!semver.satisfies(nodeRuntimeVersion, engines.node, { includePrerelease: true })) {
+        console.log(getWarningMessage(nodeRuntimeVersion, engines.node));
+    } else {
+        console.log(getSuccessMessage(nodeRuntimeVersion));
+    }
+}
+
+const getSuccessMessage = (nodeRuntimeVersion: string) => {
+    return `📦 Node version is ${nodeRuntimeVersion}. Node version check passed ✅`;
+}
+
+const getWarningMessage = (nodeRuntimeVersion: string, engines: string) => {
+    return `⚠️ Warning: Node version check failed ❌ 
             📦 Current Node version (${nodeRuntimeVersion}) does not match the required version of Node (${engines}). 
             💡 Check Readme for how to setup the correct Node version. 
-            🔗 More info at https://github.com/endatix/endatix/tree/main/apps/endatix-hub`);
-    } else {
-        console.log(`📦 Node version is ${nodeRuntimeVersion}. Node version check passed ✅`);
-    }
+            🔗 More info at https://github.com/endatix/endatix/tree/main/apps/endatix-hub`;
 }
 
 export default checkNodeVersion;
