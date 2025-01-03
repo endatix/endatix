056ef74b17252fc90a795eae5927c38a03ff4f13
Peter Marinov <peter.marinov@outlook.com>
2025-01-03 14:13:20 +0200

    feat: CI/CD Support for the Azure Production Environment (#37)

--- apps/endatix-hub/next.config.ts
+++ apps/endatix-hub/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone", // Used to decrease the size of the application, check https://nextjs.org/docs/pages/api-reference/next-config-js/output
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     serverActions: {
       bodySizeLimit: '30mb',
