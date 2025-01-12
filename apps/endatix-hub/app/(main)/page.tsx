3c1254efd6f5077fe5774c5b74274a9f0ad2b454
Peter Marinov <peter@endatix.com>
2025-01-12 16:48:11 +0200

    Fixing broken page background

--- apps/endatix-hub/app/(main)/page.tsx
+++ apps/endatix-hub/app/(main)/page.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 const Home = () => {
   return (
     <div className="grid h-full grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="absolute top-0 right-0 left-0 bottom-0 bg-[url('/lines-and-stuff.svg')] brightness-[0.6] opacity-[0.4] dark:grayscale"></div>
+      <div className="absolute top-0 right-0 left-0 bottom-0 bg-[url('/assets/lines-and-stuff.svg')] brightness-[0.6] opacity-[0.4] dark:grayscale"></div>
       <div className="z-10 items-start flex flex-col gap-4 row-start-2 items-center sm:items-start">
         <Card className="sm:col-span-4 auto-rows-max bg-background">
           <CardHeader className="pb-3">
