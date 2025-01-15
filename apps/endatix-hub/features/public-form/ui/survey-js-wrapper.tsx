658f631ff43ca424bdb63be392aff918625a219b
Peter Marinov <peter@endatix.com>
2025-01-15 18:58:24 +0200

    fix: Images are uploaded twice (#369)

--- apps/endatix-hub/features/public-form/ui/survey-js-wrapper.tsx
+++ apps/endatix-hub/features/public-form/ui/survey-js-wrapper.tsx
@@ -1,59 +1,30 @@
-'use client'
+"use client";
 
-import { Submission } from '@/types';
-import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react';
+import { Submission } from "@/types";
+import dynamic from "next/dynamic";
 
-const SurveyComponent = dynamic(() => import('./survey-component'), {
-    ssr: false,
+const SurveyComponent = dynamic(() => import("./survey-component"), {
+  ssr: false,
 });
 
 interface SurveyJsWrapperProps {
-    definition: string;
-    formId: string;
-    submission?: Submission | undefined
+  definition: string;
+  formId: string;
+  submission?: Submission | undefined;
 }
 
-const SurveyJsWrapper = ({ formId, definition, submission }: SurveyJsWrapperProps) => {
-    const [cookiesEnabled, setCookiesEnabled] = useState(true);
-
-    const areCookiesEnabled = (): boolean => {
-        if (!navigator.cookieEnabled) {
-            return false;
-        }
-
-        if (!document.cookie) {
-            document.cookie = "fkst";
-            if (document.cookie.length === 0) {
-                return false;
-            }
-
-            document.cookie = "";
-        }
-
-        return true;
-    }
-
-    useEffect(() => {
-        const cookiesEnabled = areCookiesEnabled();
-        if (!cookiesEnabled) {
-            setCookiesEnabled(false);
-        }
-    }, [cookiesEnabled]);
-
-    return (
-        <>
-            {cookiesEnabled ? (
-                <SurveyComponent
-                    formId={formId}
-                    definition={definition}
-                    submission={submission}
-                />
-            ) : (
-                <div>Cookies are not enabled. You must enable cookies to continue.</div>
-            )}
-        </>
-    );
-}
+const SurveyJsWrapper = ({
+  formId,
+  definition,
+  submission,
+}: SurveyJsWrapperProps) => {
+  return (
+    <SurveyComponent
+      formId={formId}
+      definition={definition}
+      submission={submission}
+    />
+  );
+};
 
 export default SurveyJsWrapper;
