c2375b2916c12ee831c7d08c554f38ded84a2742
Peter Marinov <peter@endatix.com>
2025-02-06 19:47:42 +0200

    Adding prettier config and removing single quotes rules

---
+++ apps/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  semi: true,
+  arrowParens: "always",
+};
