f9bd0330074b58ac4881bed3dd73cc02223b8e14
Peter Marinov <peter@endatix.com>
2025-04-11 00:54:54 +0300

    Fixing failing tests for PartialUpdateFormHandlerTests

--- tests/Endatix.Core.Tests/UseCases/Forms/PartialUpdate/PartialUpdateFormHandlerTests.cs
+++ tests/Endatix.Core.Tests/UseCases/Forms/PartialUpdate/PartialUpdateFormHandlerTests.cs
@@ -8,12 +8,14 @@ namespace Endatix.Core.Tests.UseCases.Forms.PartialUpdate;
 public class PartialUpdateFormHandlerTests
 {
     private readonly IRepository<Form> _repository;
+    private readonly IRepository<Theme> _themeRepository;
     private readonly PartialUpdateFormHandler _handler;
 
     public PartialUpdateFormHandlerTests()
     {
         _repository = Substitute.For<IRepository<Form>>();
-        _handler = new PartialUpdateFormHandler(_repository);
+        _themeRepository = Substitute.For<IRepository<Theme>>();
+        _handler = new PartialUpdateFormHandler(_repository, _themeRepository);
     }
 
     [Fact]
@@ -21,7 +23,7 @@ public class PartialUpdateFormHandlerTests
     {
         // Arrange
         Form? notFoundForm = null;
-        var request = new PartialUpdateFormCommand(1, null, null, null);
+        var request = new PartialUpdateFormCommand(1, null, null, null, null);
         _repository.GetByIdAsync(request.FormId, Arg.Any<CancellationToken>())
                    .Returns(notFoundForm);
 
@@ -45,7 +47,9 @@ public class PartialUpdateFormHandlerTests
             Description = SampleData.FORM_DESCRIPTION_1,
             IsEnabled = true
         };
-        var request = new PartialUpdateFormCommand(1, SampleData.FORM_NAME_2, SampleData.FORM_DESCRIPTION_2, false);
+        var theme = new Theme(SampleData.TENANT_ID, "Test Theme", "{ \"background\": \"#FFFFFF\" }") { Id = 4 };
+        form.SetTheme(theme);
+        var request = new PartialUpdateFormCommand(1, SampleData.FORM_NAME_2, SampleData.FORM_DESCRIPTION_2, false, null);
         _repository.GetByIdAsync(request.FormId, Arg.Any<CancellationToken>())
                    .Returns(form);
 
@@ -59,6 +63,7 @@ public class PartialUpdateFormHandlerTests
         result.Value.Name.Should().Be(request.Name);
         result.Value.Description.Should().Be(request.Description);
         result.Value.IsEnabled.Should().Be(request.IsEnabled!.Value);
+        result.Value.ThemeId.Should().Be(theme.Id);
         await _repository.Received(1).UpdateAsync(form, Arg.Any<CancellationToken>());
     }
 
@@ -67,9 +72,20 @@ public class PartialUpdateFormHandlerTests
     {
         // Arrange
         var form = new Form(SampleData.TENANT_ID, "Test Form") { Id = 1, Name = SampleData.FORM_NAME_1, Description = SampleData.FORM_DESCRIPTION_1, IsEnabled = true };
-        var request = new PartialUpdateFormCommand(1, null, SampleData.FORM_DESCRIPTION_2, null);
+        var oldTheme = new Theme(SampleData.TENANT_ID, "Test Theme", "{ \"background\": \"#FFFFFF\" }") { Id = 3 };
+        var newTheme = new Theme(SampleData.TENANT_ID, "Test Theme", "{ \"background\": \"#000000\" }") { Id = 4 };
+        var request = new PartialUpdateFormCommand(
+           formId: 1, 
+           name: null,
+           description: SampleData.FORM_DESCRIPTION_2,
+           isEnabled: null,
+           themeId: 4
+        );
+        form.SetTheme(oldTheme);
         _repository.GetByIdAsync(request.FormId, Arg.Any<CancellationToken>())
                    .Returns(form);
+        _themeRepository.GetByIdAsync(request.ThemeId!.Value, Arg.Any<CancellationToken>())
+                    .Returns(newTheme);
 
         // Act
         var result = await _handler.Handle(request, CancellationToken.None);
@@ -81,6 +97,7 @@ public class PartialUpdateFormHandlerTests
         result.Value.Name.Should().Be(form.Name);
         result.Value.Description.Should().Be(request.Description);
         result.Value.IsEnabled.Should().Be(form.IsEnabled);
+        result.Value.ThemeId.Should().Be(newTheme.Id);
         await _repository.Received(1).UpdateAsync(form, Arg.Any<CancellationToken>());
     }
 }
