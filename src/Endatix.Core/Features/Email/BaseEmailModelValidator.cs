4996c8f7c8c0863e8709c4f33a114c4eeadae873
Dobromir Petrov <14013810+dspetrov@users.noreply.github.com>
2025-07-24 13:12:21 +0300

    feat: Set up Mailgun as email provider (#454)

--- src/Endatix.Core/Features/Email/BaseEmailModelValidator.cs
+++ src/Endatix.Core/Features/Email/BaseEmailModelValidator.cs
@@ -7,16 +7,9 @@ public class BaseEmailModelValidator : AbstractValidator<BaseEmailModel>
 {
     public BaseEmailModelValidator()
     {
-        RuleFor(x => x.From)
-            .NotEmpty()
-            .EmailAddress();
-
         RuleFor(x => x.To)
             .NotEmpty()
             .EmailAddress();
-
-        RuleFor(x => x.Subject)
-           .NotEmpty();
 
         RuleFor(x => x.Metadata)
             .NotNull();
