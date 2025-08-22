68ceb0b96d7970f3850d28d61181ce58506bcecb
Peter Marinov <peter@endatix.com>
2025-08-22 19:37:19 +0300

    Add migration for audio recorder question

---
+++ src/Endatix.Persistence.PostgreSql/Migrations/AppEntities/20250822110637_AudioRecorderQuestion.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Endatix.Persistence.PostgreSql.Migrations.AppEntities
+{
+    /// <inheritdoc />
+    public partial class AudioRecorderQuestion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE public.""CustomQuestions""
+                SET ""DeletedAt"" = CURRENT_TIMESTAMP, 
+                    ""IsDeleted"" = true,
+                    ""ModifiedAt"" = CURRENT_TIMESTAMP
+                WHERE LOWER(""Name"") LIKE '%audiorecorder%';
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE public.""CustomQuestions""
+                SET ""DeletedAt"" = NULL, 
+                    ""IsDeleted"" = false,
+                    ""ModifiedAt"" = CURRENT_TIMESTAMP
+                WHERE LOWER(""Name"") LIKE '%audiorecorder%';
+            ");
+        }
+    }
+}
