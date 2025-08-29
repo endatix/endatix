64f046ea8ad1869388bc7acb287f2e99f777a3bd
Peter Marinov <peter@endatix.com>
2025-08-29 09:55:16 +0300

    fix: Sign In endpoint validation (#477)

--- src/Endatix.Api/Endpoints/Auth/Login.LoginRequestValidator.cs
+++ src/Endatix.Api/Endpoints/Auth/Login.LoginRequestValidator.cs
@@ -17,9 +17,5 @@ public class LoginRequestValidator : Validator<LoginRequest>
         RuleFor(x => x.Email)
             .NotEmpty().WithMessage("an email is required!")
             .EmailAddress(EmailValidationMode.AspNetCoreCompatible).WithMessage("Email must be a valid email address format");
-
-        RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("a password is required!")
-            .MinimumLength(8).WithMessage("password is too short!");
     }
 }
