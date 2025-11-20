b2483aada1d40c0f1f01a4ac63a4ad2d77480967
Dobromir Petrov <14013810+dspetrov@users.noreply.github.com>
2025-11-20 10:15:38 +0200

    feat: Add SQL migration

--- src/Endatix.Persistence.SqlServer/Migrations/AppEntities/20251120075615_AddCustomExportsToTenantSettings.Designer.cs
+++ src/Endatix.Persistence.SqlServer/Migrations/AppEntities/20251120075615_AddCustomExportsToTenantSettings.Designer.cs
@@ -4,6 +4,7 @@ using Endatix.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Endatix.Persistence.SqlServer.Migrations.AppEntities
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251120075615_AddCustomExportsToTenantSettings")]
+    partial class AddCustomExportsToTenantSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
