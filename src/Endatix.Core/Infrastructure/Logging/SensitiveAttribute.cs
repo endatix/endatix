59d4fa34e1f2fe45470a66e32a03a5a11a5de481
Peter Marinov <peter@endatix.com>
2025-12-29 15:43:02 +0200

    Add PiiRedactor and SensitiveValue classes to mask sensitive data in logs

--- src/Endatix.Core/Infrastructure/Logging/SensitiveAttribute.cs
+++ src/Endatix.Core/Infrastructure/Logging/SensitiveAttribute.cs
@@ -1,6 +1,24 @@
 namespace Endatix.Core.Infrastructure.Logging;
 
-[AttributeUsage(AttributeTargets.Property)]
-public class SensitiveAttribute : Attribute
+/// <summary>
+/// Indicates that the property/field/parameter is sensitive and should be masked when logging. Use this to protect sensitive data from being logged.
+/// <example>
+/// <code>
+/// public class SomeSentiveFoo
+/// {
+///     [Sensitive(SensitivityType.Email)]
+///     public string Email { get; set; }
+/// }
+/// </code>
+/// </example>
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public sealed class SensitiveAttribute(
+    SensitivityType type = SensitivityType.Generic
+) : Attribute
 {
-} 
+    /// <summary>
+    /// The type of sensitivity for the property/field/parameter.
+    /// </summary>
+    public SensitivityType SensitivityType { get; } = type;
+}
