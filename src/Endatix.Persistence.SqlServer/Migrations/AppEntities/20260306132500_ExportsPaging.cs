c5fef08b9053b6f17c839f4d7551efbd4eec7d24
Peter Marinov <peter@endatix.com>
2026-03-06 15:28:21 +0200

    Add migrations for paging and nested loops to export form submissions

---
+++ src/Endatix.Persistence.SqlServer/Migrations/AppEntities/20260306132500_ExportsPaging.cs
@@ -0,0 +1,25 @@
+﻿using Endatix.Framework.Scripts;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Endatix.Persistence.SqlServer.Migrations.AppEntities
+{
+    /// <inheritdoc />
+    public partial class ExportsPaging : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS dbo.export_form_submissions;");
+            var script = migrationBuilder.ReadEmbeddedSqlScript("Procedures/export_form_submissions.sql");
+            migrationBuilder.Sql(script);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS dbo.export_form_submissions;");
+        }
+    }
+}
