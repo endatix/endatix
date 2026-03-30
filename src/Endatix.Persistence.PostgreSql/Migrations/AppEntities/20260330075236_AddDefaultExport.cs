221ea724fb3fe4d7ee7a2a25c04dd9c98e324761
Peter Marinov <peter@endatix.com>
2026-03-30 12:11:11 +0300

    fix: Update default export function for PostgreSQL and SQL Server (#655)

---
+++ src/Endatix.Persistence.PostgreSql/Migrations/AppEntities/20260330075236_AddDefaultExport.cs
@@ -0,0 +1,25 @@
+﻿using Endatix.Framework.Scripts;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Endatix.Persistence.PostgreSql.Migrations.AppEntities
+{
+    /// <inheritdoc />
+    public partial class AddDefaultExport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS export_form_submissions(bigint);");
+            var addExportFunctionScript = migrationBuilder.ReadEmbeddedSqlScript("Functions/export_form_submissions.sql");
+            migrationBuilder.Sql(addExportFunctionScript);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No down migration needed as we are recovering not adding the function.
+        }
+    }
+}
---
+++ src/Endatix.Persistence.SqlServer/Migrations/AppEntities/20260330081044_AddDefaultExport.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Endatix.Persistence.SqlServer.Migrations.AppEntities
+{
+    /// <inheritdoc />
+    public partial class AddDefaultExport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // No up migration needed as the function is already added in the previous migration.
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No down migration needed
+        }
+    }
+}
