adfbe263ed5df97280dd4161db435cbd48fd5828
Peter Marinov <peter@endatix.com>
2026-05-04 22:38:59 +0300

    Adding tests

--- tests/Endatix.Core.Tests/Entities/FormDependencyTypeTests.cs
+++ tests/Endatix.Core.Tests/Entities/FormDependencyTypeTests.cs
@@ -26,4 +26,29 @@ public class FormDependencyTypeTests
         // Assert
         act.Should().Throw<ArgumentException>();
     }
+
+    [Theory]
+    [InlineData("DATALIST")]
+    [InlineData("DataList")]
+    [InlineData("DaTaLiSt")]
+    public void FromCode_WithUppercaseOrMixedCase_ReturnsDataListType(string code)
+    {
+        var dependencyType = FormDependencyType.FromCode(code);
+
+        dependencyType.Should().Be(FormDependencyType.DataList);
+        dependencyType.Code.Should().Be("datalist");
+    }
+
+    [Fact]
+    public void FromCode_WithNullOrWhitespace_Throws()
+    {
+        Action nullAct = () => FormDependencyType.FromCode(null!);
+        nullAct.Should().Throw<ArgumentNullException>();
+
+        Action emptyAct = () => FormDependencyType.FromCode(string.Empty);
+        emptyAct.Should().Throw<ArgumentException>();
+
+        Action whitespaceAct = () => FormDependencyType.FromCode("   ");
+        whitespaceAct.Should().Throw<ArgumentException>();
+    }
 }
