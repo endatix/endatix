5b43d393d917866a68673224e0d6bf36db389ebb
Peter Marinov <peter@endatix.com>
2026-05-15 14:17:12 +0300

    {(azureConfig as AzureStorageConfig)

--- src/Endatix.Persistence.SqlServer/Scripts/Tools/ReplaceBlobHostnames.sql
+++ src/Endatix.Persistence.SqlServer/Scripts/Tools/ReplaceBlobHostnames.sql
@@ -1,13 +1,20 @@
--- This function replaces hostnames in JSONB columns across multiple tables. It can be used to update blob storage hostnames in the database.
+-- Replaces blob storage hostnames in JSON columns across multiple tables.
 -- Parameters:
--- - source_host: The hostname to be replaced.
--- - destination_host: The new hostname to replace with.
--- - dry_run: If true, the function will perform the updates but will raise an exception at the end to prevent committing the changes. Set to false to apply the changes permanently.
+-- - @SourceHost: The hostname to be replaced.
+-- - @DestinationHost: The new hostname to replace with.
+-- - @DryRun: If 1, counts matching rows without updating. Set to 0 to apply changes permanently.
 -- Example usage:
+-- Dry run:
 -- EXEC dbo.ReplaceBlobHostnames
---    @SourceHost = 'https://old-hostname.blob.core.windows.net/',
---    @DestinationHost = 'https://new-hostname.blob.core.windows.net/',
---    @DryRun = 1;
+--     @SourceHost = 'https://old-hostname.blob.core.windows.net/',
+--     @DestinationHost = 'https://new-hostname.blob.core.windows.net/',
+--     @DryRun = 1;
+--
+-- Apply changes:
+-- EXEC dbo.ReplaceBlobHostnames
+--     @SourceHost = 'https://old-hostname.blob.core.windows.net/',
+--     @DestinationHost = 'https://new-hostname.blob.core.windows.net/',
+--     @DryRun = 0;
 
 CREATE OR ALTER PROCEDURE dbo.ReplaceBlobHostnames
 (
@@ -25,80 +32,99 @@ BEGIN
         AffectedRows INT
     );
 
-    BEGIN TRANSACTION;
+    DECLARE @AffectedRows INT;
 
-    BEGIN TRY
-
-        -- FormDefinitions
-        UPDATE dbo.FormDefinitions
-        SET JsonData = REPLACE(
-            CAST(JsonData AS NVARCHAR(MAX)),
-            @SourceHost,
-            @DestinationHost
-        )
+    IF @DryRun = 1
+    BEGIN
+        SELECT @AffectedRows = COUNT(*)
+        FROM dbo.FormDefinitions
         WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
 
         INSERT INTO @Results
-        VALUES ('FormDefinitions', @@ROWCOUNT);
+        VALUES ('FormDefinitions', @AffectedRows);
 
-        -- Submissions
-        UPDATE dbo.Submissions
-        SET JsonData = REPLACE(
-            CAST(JsonData AS NVARCHAR(MAX)),
-            @SourceHost,
-            @DestinationHost
-        )
+        SELECT @AffectedRows = COUNT(*)
+        FROM dbo.Submissions
         WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
 
         INSERT INTO @Results
-        VALUES ('Submissions', @@ROWCOUNT);
+        VALUES ('Submissions', @AffectedRows);
 
-        -- SubmissionVersions
-        UPDATE dbo.SubmissionVersions
-        SET JsonData = REPLACE(
-            CAST(JsonData AS NVARCHAR(MAX)),
-            @SourceHost,
-            @DestinationHost
-        )
+        SELECT @AffectedRows = COUNT(*)
+        FROM dbo.SubmissionVersions
         WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
 
         INSERT INTO @Results
-        VALUES ('SubmissionVersions', @@ROWCOUNT);
+        VALUES ('SubmissionVersions', @AffectedRows);
 
-        -- FormTemplates
-        UPDATE dbo.FormTemplates
-        SET JsonData = REPLACE(
-            CAST(JsonData AS NVARCHAR(MAX)),
-            @SourceHost,
-            @DestinationHost
-        )
+        SELECT @AffectedRows = COUNT(*)
+        FROM dbo.FormTemplates
         WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
 
         INSERT INTO @Results
-        VALUES ('FormTemplates', @@ROWCOUNT);
+        VALUES ('FormTemplates', @AffectedRows);
+    END
+    ELSE
+    BEGIN
+        BEGIN TRANSACTION;
 
-        -- Return structured result
-        SELECT
-            TableName,
-            AffectedRows
-        FROM @Results;
+        BEGIN TRY
+            UPDATE dbo.FormDefinitions
+            SET JsonData = REPLACE(
+                CAST(JsonData AS NVARCHAR(MAX)),
+                @SourceHost,
+                @DestinationHost
+            )
+            WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
 
-        IF @DryRun = 1
-        BEGIN
-            ROLLBACK TRANSACTION;
-        END
-        ELSE
-        BEGIN
+            INSERT INTO @Results
+            VALUES ('FormDefinitions', @@ROWCOUNT);
+
+            UPDATE dbo.Submissions
+            SET JsonData = REPLACE(
+                CAST(JsonData AS NVARCHAR(MAX)),
+                @SourceHost,
+                @DestinationHost
+            )
+            WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
+
+            INSERT INTO @Results
+            VALUES ('Submissions', @@ROWCOUNT);
+
+            UPDATE dbo.SubmissionVersions
+            SET JsonData = REPLACE(
+                CAST(JsonData AS NVARCHAR(MAX)),
+                @SourceHost,
+                @DestinationHost
+            )
+            WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
+
+            INSERT INTO @Results
+            VALUES ('SubmissionVersions', @@ROWCOUNT);
+
+            UPDATE dbo.FormTemplates
+            SET JsonData = REPLACE(
+                CAST(JsonData AS NVARCHAR(MAX)),
+                @SourceHost,
+                @DestinationHost
+            )
+            WHERE CAST(JsonData AS NVARCHAR(MAX)) LIKE '%' + @SourceHost + '%';
+
+            INSERT INTO @Results
+            VALUES ('FormTemplates', @@ROWCOUNT);
+
             COMMIT TRANSACTION;
-        END
+        END TRY
+        BEGIN CATCH
+            IF @@TRANCOUNT > 0
+                ROLLBACK TRANSACTION;
 
-    END TRY
-    BEGIN CATCH
+            THROW;
+        END CATCH
+    END
 
-        IF @@TRANCOUNT > 0
-            ROLLBACK TRANSACTION;
-
-        THROW;
-
-    END CATCH
+    SELECT
+        TableName,
+        AffectedRows
+    FROM @Results;
 END
