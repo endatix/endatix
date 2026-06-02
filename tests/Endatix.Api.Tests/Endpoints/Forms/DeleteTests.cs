622bc41b3bb22a659759febec33a68288e92ed56
Peter Marinov <peter@endatix.com>
2026-06-02 14:20:08 +0300

    fix: Modify Delete form endpoint to return ProblemHttpResult for error paths (#760)

--- tests/Endatix.Api.Tests/Endpoints/Forms/DeleteTests.cs
+++ tests/Endatix.Api.Tests/Endpoints/Forms/DeleteTests.cs
@@ -1,5 +1,6 @@
 using FastEndpoints;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Endatix.Core.Infrastructure.Result;
 using Endatix.Core.Entities;
@@ -20,26 +21,27 @@ public class DeleteTests
     }
 
     [Fact]
-    public async Task ExecuteAsync_InvalidRequest_ReturnsBadRequest()
+    public async Task ExecuteAsync_InvalidRequest_ReturnsProblemDetails()
     {
         // Arrange
         var formId = 1L;
         var request = new DeleteFormRequest { FormId = formId };
         var result = Result.Invalid();
-        
+
         _mediator.Send(Arg.Any<DeleteFormCommand>(), Arg.Any<CancellationToken>())
             .Returns(result);
 
         // Act
-        var response = await _endpoint.ExecuteAsync(request, default);
+        var response = await _endpoint.ExecuteAsync(request, TestContext.Current.CancellationToken);
 
         // Assert
-        var badRequestResult = response.Result as BadRequest;
-        badRequestResult.Should().NotBeNull();
+        var problemResult = response.Result as ProblemHttpResult;
+        problemResult.Should().NotBeNull();
+        problemResult!.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
     }
 
     [Fact]
-    public async Task ExecuteAsync_FormNotFound_ReturnsNotFound()
+    public async Task ExecuteAsync_FormNotFound_ReturnsProblemDetails()
     {
         // Arrange
         var formId = 1L;
@@ -50,11 +52,12 @@ public class DeleteTests
             .Returns(result);
 
         // Act
-        var response = await _endpoint.ExecuteAsync(request, default);
+        var response = await _endpoint.ExecuteAsync(request, TestContext.Current.CancellationToken);
 
         // Assert
-        var notFoundResult = response.Result as NotFound;
-        notFoundResult.Should().NotBeNull();
+        var problemResult = response.Result as ProblemHttpResult;
+        problemResult.Should().NotBeNull();
+        problemResult!.StatusCode.Should().Be(StatusCodes.Status404NotFound);
     }
 
     [Fact]
@@ -70,7 +73,7 @@ public class DeleteTests
             .Returns(result);
 
         // Act
-        var response = await _endpoint.ExecuteAsync(request, default);
+        var response = await _endpoint.ExecuteAsync(request, TestContext.Current.CancellationToken);
 
         // Assert
         var okResult = response.Result as Ok<string>;
@@ -84,7 +87,7 @@ public class DeleteTests
         // Arrange
         var request = new DeleteFormRequest { FormId = 123 };
         var result = Result.Success(new Form(SampleData.TENANT_ID, "Test Form"));
-        
+
         _mediator.Send(Arg.Any<DeleteFormCommand>(), Arg.Any<CancellationToken>())
             .Returns(result);
 
