d6639ce258229a9a4935763df5a45ca3c3d8a80b
Peter Marinov <peter@endatix.com>
2026-07-13 09:44:44 +0300

    Added tests for monolingual scenarios e.g. now cultures

--- tests/Endatix.Modules.Reporting.Tests/Shared/SurveyJs/SurveyJsLocalizationHelperTests.cs
+++ tests/Endatix.Modules.Reporting.Tests/Shared/SurveyJs/SurveyJsLocalizationHelperTests.cs
@@ -92,4 +92,73 @@ public class SurveyJsLocalizationHelperTests
 
         locales.Should().Equal("default", "fr");
     }
+
+    [Fact]
+    public void ReadLocalizedStrings_WithPlainStringTitle_ReturnsDefaultLocaleValue()
+    {
+        const string questionJson = """
+            {
+              "type": "radiogroup",
+              "name": "question1",
+              "title": "My value should be set by query string parameter \"test\""
+            }
+            """;
+
+        using JsonDocument question = JsonDocument.Parse(questionJson);
+
+        IReadOnlyDictionary<string, string> localized = SurveyJsLocalizationHelper.ReadLocalizedStrings(
+            question.RootElement,
+            SurveyJsPropertyNames.Title);
+
+        localized.Should().ContainSingle()
+            .Which.Key.Should().Be(SurveyJsPropertyNames.DefaultLocale);
+        localized[SurveyJsPropertyNames.DefaultLocale]
+            .Should()
+            .Be("My value should be set by query string parameter \"test\"");
+    }
+
+    [Fact]
+    public void DiscoverLocales_WithPlainStringLocalizableProps_ReturnsDefaultOnly()
+    {
+        const string definitionJson = """
+            {
+              "pages": [
+                {
+                  "elements": [
+                    {
+                      "type": "radiogroup",
+                      "name": "question1",
+                      "title": "My value should be set by query string parameter \"test\"",
+                      "setValueIf": "{test} notempty",
+                      "setValueExpression": "{test}",
+                      "choices": [
+                        "1",
+                        "2",
+                        "3"
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+            """;
+
+        using JsonDocument definition = JsonDocument.Parse(definitionJson);
+
+        List<string> locales = SurveyJsLocalizationHelper.DiscoverLocales(definition.RootElement);
+
+        locales.Should().Equal(SurveyJsPropertyNames.DefaultLocale);
+    }
+
+    [Fact]
+    public void ReadLocalizedStrings_WithPlainStringChoice_ReturnsEmptyForChoiceElement()
+    {
+        using JsonDocument choice = JsonDocument.Parse("\"1\"");
+
+        IReadOnlyDictionary<string, string> localized = SurveyJsLocalizationHelper.ReadLocalizedStrings(
+            choice.RootElement,
+            SurveyJsPropertyNames.Text);
+
+        localized.Should().BeEmpty();
+    }
 }
