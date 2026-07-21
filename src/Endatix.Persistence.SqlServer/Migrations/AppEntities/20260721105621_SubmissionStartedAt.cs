eabcbee92345903c3bcb53b47ac67ad1d5cafda5
Peter Marinov <peter@endatix.com>
2026-07-21 13:57:44 +0300

    Add base MS SQL Server and PostgreSQL migrations

---
+++ src/Endatix.Persistence.SqlServer/Migrations/AppEntities/20260721105621_SubmissionStartedAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Endatix.Persistence.SqlServer.Migrations.AppEntities
+{
+    /// <inheritdoc />
+    public partial class SubmissionStartedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartedAt",
+                table: "Submissions",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartedAt",
+                table: "Submissions");
+        }
+    }
+}
