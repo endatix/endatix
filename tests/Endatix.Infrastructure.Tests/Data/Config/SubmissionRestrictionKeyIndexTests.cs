c92fdd0569f8240ea2f329c00988fad332314705
Peter Marinov <peter@endatix.com>
2026-07-24 13:14:53 +0300

    Add testing for the restriction key in SqlServer

--- tests/Endatix.Infrastructure.Tests/Data/Config/SubmissionRestrictionKeyIndexTests.cs
+++ tests/Endatix.Infrastructure.Tests/Data/Config/SubmissionRestrictionKeyIndexTests.cs
@@ -8,7 +8,7 @@ namespace Endatix.Infrastructure.Tests.Data.Config;
 public sealed class SubmissionRestrictionKeyIndexTests
 {
     [Fact]
-    public void PostgreSql_unique_RestrictionKey_index_excludes_soft_deleted_rows()
+    public void RestrictionKeyUniqueIndex_OnPostgreSql_ExcludesSoftDeletedRows()
     {
         using var context = AppDbContextModelInspectionFactory.CreatePostgreSqlAppDbContext();
 
@@ -19,5 +19,19 @@ public sealed class SubmissionRestrictionKeyIndexTests
 
         index.IsUnique.Should().BeTrue();
         index.GetFilter().Should().Be("\"RestrictionKey\" IS NOT NULL AND \"IsDeleted\" = false");
+    }
+
+    [Fact]
+    public void RestrictionKeyUniqueIndex_OnSqlServer_ExcludesSoftDeletedRows()
+    {
+        using var context = AppDbContextModelInspectionFactory.CreateSqlServerAppDbContext();
+
+        var index = context.Model
+            .FindEntityType(typeof(Submission))!
+            .GetIndexes()
+            .Single(i => i.GetDatabaseName() == "UX_Submissions_RestrictionKey");
+
+        index.IsUnique.Should().BeTrue();
+        index.GetFilter().Should().Be("[RestrictionKey] IS NOT NULL AND [IsDeleted] = 0");
     }
 }
